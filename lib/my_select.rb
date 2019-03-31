f60b8e1a132157ce4bb13c5ec8801f0b3f0c563e
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 05:15:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+ new_collection = []
+
+ while counter < collection.count
+   new_collection.push(yield(collection[counter]))
+   counter += 1
+ end
+
+ new_collection
 end
